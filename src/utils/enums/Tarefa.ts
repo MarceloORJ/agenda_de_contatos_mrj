b6682d8dffc1ee29624f4331a356da93802c0f06
Marcelo Oliveira <marcelo@exemplo.com>
2024-02-08 16:55:42 -0300

    Arquivos iniciais

---
+++ src/utils/enums/Tarefa.ts
@@ -0,0 +1,10 @@
+export enum Prioridade {
+  URGENTE = 'urgente',
+  IMPORTANTE = 'importnate',
+  NORMAL = 'normal'
+}
+
+export enum Status {
+  PENDENTE = 'pendente',
+  CONCLUIDA = 'concluida'
+}
